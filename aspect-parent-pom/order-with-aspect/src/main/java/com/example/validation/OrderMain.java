30475de1a715d9b9ebac7064b943ca6a4da7917e
Huang Xiao <youngledo@qq.com>
2025-06-25 17:05:27 +0800

    fix：修复“validation-aspect”模块没有增加织入，导致“java.lang.NoSuchMethodError”错误。

---
+++ aspect-parent-pom/order-with-aspect/src/main/java/com/example/validation/OrderMain.java
@@ -0,0 +1,13 @@
+package com.example.validation;
+
+///
+/// @author Huang Xiao
+/// @version 1.0.0
+/// @since 2025/6/25 16:01
+///
+
+public class OrderMain {
+    public static void main(String[] args) {
+        new Order().validateInternalState();
+    }
+}
